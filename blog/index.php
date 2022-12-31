b201b8494e815c62f7821509575a6431aa2b5fd9
Elianamos <elianamos29@gmail.com>
2022-12-31 19:25:20 +0300

    add intial file

---
+++ blog/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>A blog application</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    </head>
+    <body>
+        <h1>Blog title</h1>
+        <p>This paragraph summarises what the blog is about.</p>
+        <?php for ($postId = 1; $postId <= 3; $postId++): ?>
+            <h2>Article <?php echo $postId ?> title</h2>
+            <div>dd Mon YYYY</div>
+            <p>A paragraph summarising article <?php echo $postId ?>.</p>
+            <p>
+                <a href="#">Read more...</a>
+            </p>
+        <?php endfor ?>
+    </body>
+</html>
